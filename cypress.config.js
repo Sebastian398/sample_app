603899a715aa1b924f0ad18c680a1773e9180d74
Sebastian <neider.ramirezdelgadillo@gmail.com>
2025-06-24 20:50:27 -0500

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require("cypress");
 module.exports = defineConfig({
  e2e: {
- baseUrl: 'http://app:5050/login',
+ baseUrl: 'http://localhost:5050/login',
  supportFile: false,
  specPattern: 'cypress/e2e/**/*.js'
  }
