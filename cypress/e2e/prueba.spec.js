a1a3cd0f9c6cc410e1a3ea7cb68677606cf7ddca
Sebastian <neider.ramirezdelgadillo@gmail.com>
2025-06-25 21:37:24 -0500

    Update prueba.spec.js

--- cypress/e2e/prueba.spec.js
+++ cypress/e2e/prueba.spec.js
@@ -1,6 +1,6 @@
 describe('Prueba de login', () => {
  it('verifica que el login funcione', () => {
- cy.visit('/login');
+ cy.visit('');
  cy.get('#username').type('admin');
  cy.get('#password').type('admin123');
  cy.get('button[type="submit"]').click();
