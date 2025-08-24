69ef68de862d6086316bf08f7b13e9ad41bd90dd
neider <neider.ramirezdelgadillo@gmail.com>
2025-08-23 20:55:41 -0500

    Se actualizan los archivos correctamente

--- e2e/cypress.config.js
+++
@@ -1,8 +0,0 @@
-const { defineConfig } = require("cypress");
-module.exports = defineConfig({
- e2e: {
- baseUrl: 'http://localhost:5050',
- supportFile: false,
- specPattern: 'cypress/e2e/**/*.js'
- }
-});
